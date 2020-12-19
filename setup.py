cd214c051135c2b4f80f45b473b040868837c776
Ahmed <ahmedhamedaly19@gmail.com>
2020-12-19 14:33:35 +0000

    pip setuptools

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="example-pkg-YOUR-USERNAME-HERE", # Replace with your own username
+    name="AdvertsAPI",
     version="0.0.1",
-    author="Example Author",
-    author_email="author@example.com",
-    description="A small example package",
+    author="Ahmed Hamed Aly",
+    author_email="hamedala@tcd.ie",
+    description="An unofficial API for the community based Irish marketplace, adverts.ie",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/pypa/sampleproject",
+    url="https://github.com/ahmedhamedaly/Adverts-API",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
