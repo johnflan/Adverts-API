6aa92cbfa8498079713000415f3667b9b5763010
Ahmed <ahmedhamedaly19@gmail.com>
2020-12-20 15:57:04 +0000

    login feature implemented

--- AdvertsAPI/__init__.py
+++ AdvertsAPI/__init__.py
@@ -28,20 +28,20 @@ class AdvertsAPI:
         self.view = view
         self.keywords = keywords
         self.url = self.__generate_url()
+        self.cj = http.cookiejar.CookieJar()
+        self.br = mechanize.Browser()
+
 
     
     def login(self, username, password):
-        # login (somehow)
-        cj = http.cookiejar.CookieJar()
-        br = mechanize.Browser()
-        br.set_handle_robots(False)
-        br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
-        br.set_cookiejar(cj)
-        br.open(self.__login_url)
-        br.select_form(id="login_form")
-        br.form['authuser'] = username
-        br.form['password'] = password
-        response = br.submit()
+        self.br.set_handle_robots(False)
+        self.br.addheaders = [('User-agent', 'Mozilla/5.0 (X11; U; Linux i686; en-US; rv:1.9.0.1) Gecko/2008071615 Fedora/3.0.1-1.fc9 Firefox/3.0.1')]
+        self.br.set_cookiejar(self.cj)
+        self.br.open(self.__login_url)
+        self.br.select_form(id="login_form")
+        self.br.form['authuser'] = username
+        self.br.form['password'] = password
+        response = self.br.submit()
 
         if "        Sorry, we don't recognize that email or password. Please try again!    " in response.read().decode('utf-8'):
             print('unsuccessful')
